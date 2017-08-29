d409c3fb630e855740ab5810d37a1d2b6afce46b
App Academy Student <student@appacademy.io>
2017-08-29 11:58:17 -0700

    Add Sub table and model

--- RedditClone/app/models/user.rb
+++ RedditClone/app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true }
 
   after_initialize :ensure_session_token
-  
+
+  has_many :subs,
+  primary_key: :id,
+  foreign_key: :moderator_id,
+  class_name: :Sub
+
   attr_reader :password
 
   def password=(pw)
